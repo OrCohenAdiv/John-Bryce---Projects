89e4ee3a6dc576b6c97bae4a5cbac6bb78ae87f3
Or Cohen-Adiv <orcohenadiv@gmail.com>
2021-05-30 16:27:51 +0300

    intro to reactive

---
+++ Day16/Fun-With-RxJS/src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { observable, Observer, Subject } from 'rxjs';
+import { interval, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Fun-With-RxJS';
+  createIntervalObservable(): Observable<number> {
+    return interval(1000);
+  }
+
+  createCustomObservable(): Observable<number> {
+    return new Observable<number>(observer => {
+      observer.next(100);
+      setTimeout(() => observer.next(200), 2000);
+      setTimeout(() => observer.next(300), 5000);
+      setTimeout(() => observer.next(400), 8000);
+      setTimeout(() => observer.complete(), 10000);
+    })
+  }
+
+  createCustomSubject(): Observable<number> {
+    let res = new Subject<number>();
+    res.next(100);
+    setTimeout(() => res.next(200), 2000);
+    setTimeout(() => res.next(300), 5000);
+    setTimeout(() => res.next(400), 8000);
+    setTimeout(() => res.complete(), 10000);
+
+    return res;
+
+  }
+
+  createCustomBehaviorSubject(): Observable<number> {
+    let res = new BehaviorSubject<number>(100);
+    res.next(100);
+    setTimeout(() => res.next(200), 2000);
+    setTimeout(() => res.next(300), 5000);
+    setTimeout(() => res.next(400), 8000);
+    setTimeout(() => res.complete(), 10000);
+
+    return res;
+  }
+
+  createObserver(id: string): Observer<number> {
+    return {
+      next: val => console.log(`observer ${id}: next ${val}`),
+      complete: () => console.log(`observer ${id}: completed`),
+      error: err => console.log(`observer ${id}: error ${err}`)
+    };
+  }
+
+  go() {
+    let observer1 = this.createObserver('A');
+    let observer2 = this.createObserver('B');
+
+    let observable = this.createCustomBehaviorSubject();
+
+    observable.subscribe(observer1);
+
+    setTimeout(() => {
+      observable.subscribe(observer2);
+    }, 2500);
+  }
+}
---
+++ Day16/Fun-With-RxJS/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CounterWriterComponent } from './components/counter-writer/counter-writer.component';
+import { CounterReaderComponent } from './components/counter-reader/counter-reader.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterWriterComponent,
+    CounterReaderComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
